88d39e72cc771652136e21b748f43e02190328b9
Paula Waite <pawaite@madisoncollege.edu>
2016-09-29 10:29:13 -0500

    adding week 5 day 2

--- src/java111/project2/demos/PolicyTestDrive.java
+++ src/java111/project2/demos/PolicyTestDrive.java
@@ -12,59 +12,7 @@
       * @param the command line arguments
       */
      public static void main(String[] args) {
-     
-        // instantiate a policy object
-        // Policy is the type
-        // policy1 is the object reference variable
-        Policy policy1 = new Policy();
-        Policy policy2 = new Policy();
-        
-        // Create an array to hold 3 policies
-        
-        Policy[] policies = new Policy[3];
-        
-        // set each instance variable
-        policy1.setPolicyHolderName("Darci Donaldson");
-        policy1.setPolicyNumber("74-A-123-BC");
-        policy1.setPremium(500.00);
-        policy1.setTermInMonths(12);
-        policy1.setPolicyType("Auto");
-        policy1.setInsuredItem("Shelby");
-        policy1.setInsuredName(policy1.getPolicyHolderName());
-        
-        // set each instance variable
-        policy2.setPolicyHolderName("Mickey Mouse");
-        policy2.setPolicyNumber("75-123-BC");
-        policy2.setPremium(250.00);
-        policy2.setTermInMonths(6);
-        policy2.setPolicyType("Auto");
-        policy2.setInsuredItem("Chevy");
-        policy2.setInsuredName(policy2.getPolicyHolderName());
-       
-        // put the policies in the array
-        policies[0] = policy1;
-        policies[1] = policy2;
-        
-        policies[2] = new Policy();
-        policies[2].setPolicyHolderName("Rebecca");
-        policy2.setInsuredItem("Jeep Renegade");
-        
-        // call the changePremium method passing in 50.00
-        policy1.changePremium(50.00, false);
-        
-        // call the display method on the object
-        
-        
-        for (int counter = 0;counter < policies.length;counter++) {
-            policies[counter].display();
-            System.out.println();
-            System.out.println("*********************");
-            System.out.println();
-            // policies[0] is the equivalent of policy1
-            // policies[0].display is the same as policy1.display
-        }  
-        
-        
-     
+         BookOfBusinessProcessor processor = new BookOfBusinessProcessor();
+         processor.run();
      }
  }
