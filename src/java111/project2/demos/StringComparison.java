d34a9545eb448699db7ba326387e25702e4fd76c
Paula Waite <pawaite@madisoncollege.edu>
2016-10-03 16:00:13 -0500

    adding week 6 day 1

---
+++ src/java111/project2/demos/StringComparison.java
@@ -0,0 +1,26 @@
+/** Demonstrating String comparisons
+ *  @author pwaite
+ */
+ 
+ public class StringComparison {
+ 
+     public static void main(String[] args) {
+         String myStringOne = "Hello";
+         String myStringTwo = "Goodbye";
+         
+         if (myStringOne.equals(myStringTwo)) {
+             System.out.println("The values are equal");
+         } else {
+             System.out.println("The values are not equal");
+         }
+         
+         
+     
+     
+     
+     }
+ 
+ 
+ 
+ 
+ }
