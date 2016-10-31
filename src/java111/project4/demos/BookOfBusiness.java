1abdda3cdfd2e07e8b79328c18f435fd0f5e5cde
Paula Waite <pawaite@madisoncollege.edu>
2016-10-31 16:20:00 -0500

    Week 10 day 1

---
+++ src/java111/project4/demos/BookOfBusiness.java
@@ -0,0 +1,128 @@
+import java.util.*;
+
+/** Represents and agent's book of business - his/her policies
+ * 
+ * @author pwaite
+ */
+
+ public class BookOfBusiness {
+    private String agentName; 
+    //private Policy[] policies;
+    private ArrayList<Policy> policies;
+    
+
+	/**
+
+	 * Returns the value of agentName.
+	 * @return agent's name
+
+	 */
+
+	public String getAgentName() {
+
+		return agentName;
+
+	}
+
+
+
+
+
+	/**
+
+	 * Sets the value of agentName.
+
+	 * @param agentName The value to assign agentName.
+
+	 */
+
+	public void setAgentName(String agentName) {
+
+		this.agentName = agentName;
+
+	}
+
+
+
+
+
+	/**
+
+	 * Returns the value of policies.
+
+	 * @return agent's list of policies
+	 */
+
+	public ArrayList<Policy> getPolicies() {
+
+		return policies;
+
+	}
+
+
+
+
+
+	/**
+
+	 * Sets the value of policies.
+
+	 * @param policies The value to assign policies.
+
+	 */
+
+	public void setPolicies(ArrayList<Policy> policies) {
+
+		this.policies = policies;
+
+	}
+
+	
+	/** 
+	 * displays the book of business (each policy and the agent's name)
+	 */
+	 
+	 public void display() {
+	 
+	     System.out.println("Agent " + agentName + " Book of Business");
+	     System.out.println("***********************************");
+	     System.out.println();
+	     for (int counter = 0; counter < policies.size(); counter++) {
+	         policies.get(counter).display();
+	         System.out.println();
+	     }
+	 
+	 }
+	 
+	 public void displayUsingEnhancedFor() {
+	
+	     System.out.println("Agent " + agentName + " Book of Business");
+	     System.out.println("***********************************");
+	     System.out.println();
+	     // accessing the arraylist
+	     // policies is the arraylist (the collection I'm iterating over)
+	     // policy is a variable for each item in the arraylist
+	     // read this as: for each policy in the policies arraylist display that policy
+	     for (Policy policy : policies ) {
+	         policy.display();
+	         System.out.println();
+	     }
+	 
+	 }
+
+
+	
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
---
+++ src/java111/project4/demos/BookOfBusinessProcessor.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+/** Class to create the book of business and the policies in that book.
+ * @author pwaite
+ */
+ 
+ public class BookOfBusinessProcessor {
+     private BookOfBusiness book;
+     
+	/**
+	 * Returns the value of book.
+	 * @return the book of business
+	 */
+
+	public BookOfBusiness getBook() {
+		return book;
+	}
+
+	/**
+	 * Sets the value of book.
+	 * @param book The value to assign book.
+	 */
+
+	public void setBook(BookOfBusiness book) {
+		this.book = book;
+	}
+	
+	/** main method to process the book of business
+	 */
+	 
+	 public void run(){
+	    // instantiate the book of business object and 
+	    // assign it to the instance variable
+	    createBook();
+	    createPolicies();
+	    displayBook();
+ 
+	 }
+	 
+	 public void createBook() {
+	   // BookOfBusiness book = new BookOfBusiness(); // creates a local variable only
+	   
+	    book = new BookOfBusiness(); // creates the object for the instance variable
+	   
+	    book.setAgentName("Sam"); 
+	 }
+	 
+	 /** create agent's policies */
+	 public void createPolicies() {
+	    // instantiate a policy object
+        // Policy is the type
+        // policy1 is the object reference variable
+        Policy policy1 = new Policy();
+        Policy policy2 = new Policy();
+        Policy policy3 = new Policy();
+        
+        // Create an array to hold 3 policies
+        
+        //Policy[] policies = new Policy[3];
+        ArrayList<Policy>  policies = new ArrayList<Policy>();
+        
+        // set each instance variable
+        policy1.setPolicyHolderName("Darci Donaldson");
+        policy1.setPolicyNumber("74-A-123-BC");
+        policy1.setPremium(500.00);
+        policy1.setTermInMonths(12);
+        policy1.setPolicyType("Auto");
+        policy1.setInsuredItem("Shelby");
+        policy1.setInsuredName(policy1.getPolicyHolderName());
+        
+        // set each instance variable
+        policy2.setPolicyHolderName("Mickey Mouse");
+        policy2.setPolicyNumber("75-123-BC");
+        policy2.setPremium(250.00);
+        policy2.setTermInMonths(6);
+        policy2.setPolicyType("Auto");
+        policy2.setInsuredItem("Chevy");
+        policy2.setInsuredName(policy2.getPolicyHolderName());
+        
+        // set each instance variable
+        policy3.setPolicyHolderName("Daffy Duck");
+        policy3.setPolicyNumber("75-123-BC");
+        policy3.setPremium(250.00);
+        policy3.setTermInMonths(6);
+        policy3.setPolicyType("Auto");
+        policy3.setInsuredItem("Some Goofy Car");
+        policy3.setInsuredName(policy3.getPolicyHolderName());
+       
+       
+        // put the policies in the array
+        policies.add(policy1);
+        policies.add(policy2);
+        policies.add(policy3);
+        
+        // set the array of policies into the book of business
+        book.setPolicies(policies);
+ 	     
+	 }
+	 
+	 public void displayBook() {
+	     book.display();
+	 
+	 }
+	 
+
+
+
+	
+     
+     
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
