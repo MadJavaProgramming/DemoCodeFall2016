cf634a6db97af81c6d68a66dcb5784e489868850
Paula Waite <pawaite@madisoncollege.edu>
2016-11-28 15:23:30 -0600

    adding staticTestDrive

---
+++ src/java111/project5/StaticTestDrive.java
@@ -0,0 +1,15 @@
+package java111.project5;
+
+public class StaticTestDrive{
+    
+    //int count;
+    
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+        //System.out.println(count); - cannot access this instance variable in a static method because the var is not static
+    
+    }
+
+}
+
+
